28b3403d4761e97d0c55977bdb8024d7f4163d39
ItsjustSteveC <scubi.curtis@gmail.com>
2024-01-11 22:11:30 +1000

    Adding three project files

---
+++ README.md
@@ -0,0 +1,3 @@
+## The Unix Workbench assessment of Steve Curtis
+Today is $(date '+%Y-%m-%d')
+The number of lines of code in guessinggame.sh is 23
---
+++ guessinggame.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+files=3
+
+echo "This is a guessing game"
+echo "Guess how many files are in the directory"
+
+response=0 
+
+while [[ $response -ne $files ]] ; do
+	read response
+	[[ 0$response -gt $files ]] && echo "Your guess is too high, try again"
+ 	[[ 0$response -lt $files ]] && echo "your guess is too low. try again"
+
+done
+
+echo "You guessed the correct number, Congratulations"
+echo "Thank you for playing"
+
+exit 0
+
+
